abb88c8439546a5714bf99080df54570425cf2b5
anitamrazyanz59 <anitamrazyanz59@gmail.com>
2022-10-12 14:51:33 +0400

    update README.md

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+Created simple product application based on Vue js Curses first and second lessons.
+In project was used vuex, tailwind, vue-router packages
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/Home";
-import Category from "@/components/layouts/Category";
 import AllProductsView from "@/views/AllProductsView";
 import ProductView from "@/views/ProductView";
 import CartView from "@/views/CartView";
@@ -37,15 +36,7 @@ const routes = [
     path: '/cart',
     name: 'cart',
     component: CartView
-  },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+  }
 ]
 
 const router = createRouter({
