4f32b7e15b8ab3dde9e8ce8e7c5d5c95ef8ac4ae
anitamrazyanz59@gmail.com <as@sky-mechanics.com>
2022-10-12 14:34:06 +0400

    product app

---
+++ src/components/layouts/AllCategories.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Category v-for="(categoryItem, key) in categories" :category="categoryItem" :key="key">
+
+    </Category>
+  </div>
+</template>
+
+<script>
+import {mapState} from "vuex";
+import Category from "@/components/layouts/Category";
+
+export default {
+  computed: {
+    ...mapState(['categories'])
+  },
+  components: {
+    Category
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
